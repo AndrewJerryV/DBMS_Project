57897cc9809f30cf20bfd5e41183f99a8a6f98bf
Jishnu Vijayan <178360970+jisnu-glitch@users.noreply.github.com>
2025-10-03 01:46:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password@123',
-  database: 'dbm_proj'
+  database: 'DBMS_Project'
 });
 
 db.connect(err => {
